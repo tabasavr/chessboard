78185f3674c1314c4389020422a4a7835309b072
Sergei Kozelko <kozelkos@yandex.ru>
2022-08-08 18:33:08 +0300

    Add descriptions

--- chessboard/src/main/java/dev/rsbat/chessboard/Board.kt
+++ chessboard/src/main/java/dev/rsbat/chessboard/Board.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 
 /**
@@ -36,9 +37,10 @@ fun Board(boardData: Array<Array<Piece>>, modifier: Modifier = Modifier) {
                                 it.testTag("row $rowIdx col $colIdx")
                             } else { it } }
                     ) {
+                        val piece = boardData[rowIdx][colIdx]
                         Image(
-                            painter = painterResource(id = boardData[rowIdx][colIdx].drawableRes),
-                            contentDescription = "Bishop",
+                            painter = painterResource(id = piece.drawableRes),
+                            contentDescription = stringResource(piece.descriptionRes),
                             modifier = Modifier.fillMaxSize(),
                             alignment = Alignment.Center,
                         )
--- chessboard/src/main/java/dev/rsbat/chessboard/Piece.kt
+++ chessboard/src/main/java/dev/rsbat/chessboard/Piece.kt
@@ -1,38 +1,40 @@
 package dev.rsbat.chessboard
 
 import androidx.annotation.DrawableRes
-import dev.rsbat.chessboard.R
+import androidx.annotation.StringRes
 
 /**
  * Enum used for defining the board
+ * @param drawableRes piece icon resource
+ * @param descriptionRes text description resource
  */
-enum class Piece(@DrawableRes val drawableRes: Int) {
+enum class Piece(@DrawableRes val drawableRes: Int, @StringRes val descriptionRes: Int) {
     /** No piece **/
-    None(R.drawable.ic_chess_none),
+    None(R.drawable.ic_chess_none, R.string.piece_none),
 
     /** White bishop **/
-    BishopWhite(R.drawable.ic_chess_blt45),
+    BishopWhite(R.drawable.ic_chess_blt45, R.string.piece_white_bishop),
     /** White king **/
-    KingWhite(R.drawable.ic_chess_klt45),
+    KingWhite(R.drawable.ic_chess_klt45, R.string.piece_white_king),
     /** White knight **/
-    KnightWhite(R.drawable.ic_chess_nlt45),
+    KnightWhite(R.drawable.ic_chess_nlt45, R.string.piece_white_knight),
     /** White pawn **/
-    PawnWhite(R.drawable.ic_chess_plt45),
+    PawnWhite(R.drawable.ic_chess_plt45, R.string.piece_white_pawn),
     /** White queen **/
-    QueenWhite(R.drawable.ic_chess_qlt45),
+    QueenWhite(R.drawable.ic_chess_qlt45, R.string.piece_white_queen),
     /** White rook **/
-    RookWhite(R.drawable.ic_chess_rlt45),
+    RookWhite(R.drawable.ic_chess_rlt45, R.string.piece_white_rook),
 
     /** Black bishop **/
-    BishopBlack(R.drawable.ic_chess_bdt45),
+    BishopBlack(R.drawable.ic_chess_bdt45, R.string.piece_black_bishop),
     /** Black king **/
-    KingBlack(R.drawable.ic_chess_kdt45),
+    KingBlack(R.drawable.ic_chess_kdt45, R.string.piece_black_king),
     /** Black knight **/
-    KnightBlack(R.drawable.ic_chess_ndt45),
+    KnightBlack(R.drawable.ic_chess_ndt45, R.string.piece_black_knight),
     /** Black pawn **/
-    PawnBlack(R.drawable.ic_chess_pdt45),
+    PawnBlack(R.drawable.ic_chess_pdt45, R.string.piece_black_pawn),
     /** Black queen **/
-    QueenBlack(R.drawable.ic_chess_qdt45),
+    QueenBlack(R.drawable.ic_chess_qdt45, R.string.piece_black_queen),
     /** Black rook **/
-    RookBlack(R.drawable.ic_chess_rdt45),
+    RookBlack(R.drawable.ic_chess_rdt45, R.string.piece_black_rook),
 }
