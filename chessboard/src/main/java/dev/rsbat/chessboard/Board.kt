302fbb6e6ba0fc4b253a20c334c450822fbc74a8
Sergei Kozelko <kozelkos@yandex.ru>
2022-08-21 19:54:21 +0300

    Remove wildcard import

--- chessboard/src/main/java/dev/rsbat/chessboard/Board.kt
+++ chessboard/src/main/java/dev/rsbat/chessboard/Board.kt
@@ -2,7 +2,11 @@ package dev.rsbat.chessboard
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
